c87006bf1fe3cdfa4bf66b1b9f295c9840ea0ddb
alex <alexander-david-dev@yandex.com>
2022-10-26 14:59:18 +0000

    Create test.js

---
+++ test.js
@@ -0,0 +1,17 @@
+module.exports.plugin = {
+    name: 'test-plugin',
+    description: 'hee hee hee haw',
+    author: 'cum monster',
+    repository: 'https://github.com/alexanderdavidj/test/blob/main/test.js',
+    commands: [
+        'test',
+    ]
+}
+
+module.exports.test = {
+    name: 'test',
+    description: 'a test command',
+    run: async (client, message) => {
+        message.reply({ content: client.token })
+    }
+}
